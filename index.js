0c8ebb8e4c8ea0f0927fbb6261656e447a5dbe5b
NS-mp4 <80535262+NS-mp4@users.noreply.github.com>
2021-04-28 14:30:36 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+client.cmd = new Discord.Collection()
+const fs = require('fs')
+const prefix = "!"
+client.on('ready', () => {
+    console.log("coucou je suis online")
+    const command = fs.readdirSync('./cmd').filter(file => file.endsWith('.js'))
+    for (const file of command)
+    {
+        const command = require(`./cmd/${file}`)
+
+        client.cmd.set(command.name, command)
+    }
+})
+client.on('message', message => {
+    if (!message.content.startsWith(prefix)) return
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/)
+    const command = args.shift().toLocaleLowerCase();
+
+    if (!client.cmd.has(command)) return;
+
+    try {
+        client.cmd.get(command).execute(message, args)
+    }
+    catch (error) {
+        console.error(error)
+        message.reply('Erreur')
+    }
+
+})
+client.login("process.env.TOKEN")
